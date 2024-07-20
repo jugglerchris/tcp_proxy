36e1334ef62e07e0800d9706581eac12f53a0b68
Chris Emerson <github@mail.nosreme.org>
2024-07-20 10:37:48 +0100

    Allow list added.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::net::Ipv4Addr;
+
 use anyhow::bail;
 use clap::Parser;
 use tokio::net::TcpListener;
@@ -11,15 +13,33 @@ struct Cli {
     /// The address to forward to
     #[arg(long)]
     fwd_addr: Option<String>,
+
+    /// A list of allowed source addresses
+    #[arg(long)]
+    allow_addr: Vec<Ipv4Addr>,
 }
 
-async fn run_listener(bind_addr: &str, fwd_addr: &str) -> anyhow::Result<()> {
+async fn run_listener(bind_addr: &str, fwd_addr: &str,
+                      allow_addr: &[Ipv4Addr]) -> anyhow::Result<()> {
     let listener = TcpListener::bind(bind_addr).await?;
 
     loop {
         match listener.accept().await {
             Ok((_sock, addr)) => {
                 println!("New client: {:?}", addr);
+                match addr.ip() {
+                    std::net::IpAddr::V4(addr4) => {
+                        if allow_addr.iter()
+                            .any(|a| *a == addr4) {
+                            println!("Allowed address");
+                        } else {
+                            println!("Address not on allow list");
+                        }
+                    }
+                    std::net::IpAddr::V6(_) => {
+                        eprintln!("IPv6 not supported.");
+                    }
+                }
             }
             Err(e) => {
                 eprintln!("Error in accept: {e}");
@@ -47,5 +67,5 @@ async fn main() -> anyhow::Result<()> {
         }
     };
 
-    run_listener(&bind_addr, &fwd_addr).await
+    run_listener(&bind_addr, &fwd_addr, &cli.allow_addr).await
 }
