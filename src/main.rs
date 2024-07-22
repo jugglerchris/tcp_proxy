e0ddd359cec3ec0306da25d064947c9710f1a410
Chris Emerson <github@mail.nosreme.org>
2024-07-22 18:24:37 +0100

    Add tracing.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::net::{Ipv4Addr, SocketAddr};
 use anyhow::bail;
 use clap::Parser;
 use tokio::net::{lookup_host, tcp, TcpListener, TcpStream};
+use tracing::{debug, debug_span, error, info, trace, Instrument};
 
 #[derive(Parser)]
 struct Cli {
@@ -22,26 +23,28 @@ struct Cli {
 async fn do_proxy_oneway(mut reader: tcp::OwnedReadHalf, mut writer: tcp::OwnedWriteHalf) {
     match tokio::io::copy(&mut reader, &mut writer).await {
         Ok(count) => {
-            println!("Finished proxying {count} bytes");
+            debug!("Finished proxying {count} bytes");
         }
         Err(e) => {
-            eprintln!("Proxy error: {e}");
+            error!("Proxy error: {e}");
         }
     }
 }
 
 async fn do_proxy(dest: &SocketAddr, sock: TcpStream, peer: Ipv4Addr) {
     let dest_result = TcpStream::connect(dest).await; 
-    println!("Connecting to {peer:?}");
+    info!("Connecting to {peer:?}");
     match dest_result {
         Ok(dest_sock) => {
             let (dest_read, dest_write) = dest_sock.into_split();
             let (src_read, src_write) = sock.into_split();
-            tokio::spawn(do_proxy_oneway(dest_read, src_write));
-            tokio::spawn(do_proxy_oneway(src_read, dest_write));
+            tokio::spawn(do_proxy_oneway(dest_read, src_write)
+                         .instrument(debug_span!("copy1")));
+            tokio::spawn(do_proxy_oneway(src_read, dest_write)
+                         .instrument(debug_span!("copy2")));
         }
         Err(e) => {
-            eprintln!("Error connecting: {e}");
+            error!("Error connecting: {e}");
         }
     }
 }
@@ -51,29 +54,33 @@ async fn run_listener(bind_addr: &str, fwd_addr: &str,
     let listener = TcpListener::bind(bind_addr).await?;
 
     let dest = lookup_host(fwd_addr).await?.next().unwrap();
-    eprintln!("dest = {dest:?}");
+    debug!("dest = {dest:?}");
 
+    let mut accepts = 0;
     loop {
         match listener.accept().await {
             Ok((sock, addr)) => {
-                println!("New client: {:?}", addr);
+                accepts += 1;
+                debug!("New client {accepts}: {:?}", addr);
                 match addr.ip() {
                     std::net::IpAddr::V4(addr4) => {
                         if allow_addr.iter()
                             .any(|a| *a == addr4) {
-                            println!("Allowed address");
-                            do_proxy(&dest, sock, addr4).await;
+                            trace!("Allowed address");
+                            do_proxy(&dest, sock, addr4)
+                                .instrument(debug_span!("do_proxy", num = accepts))
+                                .await;
                         } else {
-                            println!("Address not on allow list");
+                            info!("Address {addr:?} not on allow list");
                         }
                     }
                     std::net::IpAddr::V6(_) => {
-                        eprintln!("IPv6 not supported.");
+                        debug!("IPv6 not supported.");
                     }
                 }
             }
             Err(e) => {
-                eprintln!("Error in accept: {e}");
+                info!("Error in accept: {e}");
             }
         }
     }
@@ -82,6 +89,7 @@ async fn run_listener(bind_addr: &str, fwd_addr: &str,
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
     let cli = Cli::parse();
 
     let bind_addr = match cli.bind_addr {
